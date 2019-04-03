04fa7ede0b777e428e9bff0e839df984cf0bded2
Saijitsu <zeromus16@gmail.com>
2019-04-03 12:29:54 +0200

    alert on XSS test

--- express/index.js
+++ express/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+const njk = require('nunjucks').configure(`${__dirname}/template`, {
+    autoescape: true,  // pour eviter les injection XSS, n'interprete pas par defaut le code en html (balise off)
+    // pour éviter les injection d'SQL il suffira d'éviter les " et ' en ne les interpretant pas (échapper les caractères).
+    // CSRF (forger un requête sur un server distant), suppression via un lien (envoyé a un utilisateur ayant les droits):
+    // - utilisation d'un capcha
+    // - utilisation de token (acces temporaire, via système de token), le server le crée sur demande de l'utilisateur
+    // Enfin => HTTPS (une couche de chiffrement comme SSL ou TLS se rajoute)
+    express: app
+});
 
 app.use(express.static(__dirname)); // fichier static
 
 app.get('/', (request, response) => {
+
+    let userSource = "<script>alert('xss')</script>";
+
+    response.render('accueil.njk.html', { html: userSource });
+
+});
+
+app.get('/old', (request, response) => {
 
     let html = `
     <html>
